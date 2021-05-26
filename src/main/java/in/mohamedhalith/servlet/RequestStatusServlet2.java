e17fd7b2370228ccb0195fbd88556d9e4e91d130
MohamedHalith001 <mohamedhalith1994@gmail.com>
2021-05-26 17:05:08 +0530

    Fixed code smells

---
+++ src/main/java/in/mohamedhalith/servlet/RequestStatusServlet2.java
@@ -0,0 +1,52 @@
+package in.mohamedhalith.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import in.mohamedhalith.exception.ServiceException;
+import in.mohamedhalith.exception.ValidationException;
+import in.mohamedhalith.model.LeaveRequest;
+import in.mohamedhalith.service.LeaveRequestService;
+import in.mohamedhalith.util.LocalDateAdapter;
+import in.mohamedhalith.util.LocalDateTimeAdapter;
+
+/**
+ * Servlet implementation class RequestStatusServlet2
+ */
+@WebServlet("/RequestStatusServlet2")
+public class RequestStatusServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    /**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		int employeeId = (Integer) session.getAttribute("employeeId");
+		try {
+			List<LeaveRequest> employeeRequests = LeaveRequestService.getEmployeeRequests(employeeId);
+			Gson gson = new GsonBuilder().setPrettyPrinting()
+			        .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+			        .registerTypeAdapter(LocalDate.class, new LocalDateAdapter()).create();
+			String json = gson.toJson(employeeRequests);
+			PrintWriter out = response.getWriter();
+			out.print(json);
+			out.flush();
+		} catch (ServiceException | ValidationException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/webapp/viewrequeststatus-json.jsp
@@ -0,0 +1,55 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="ISO-8859-1">
+<title>Request status</title>
+</head>
+<body>
+	<jsp:include page="header.jsp"></jsp:include>
+	<main class="container-fluid">
+		<h3>Request Status</h3>
+		<table class="table table-bordered">
+			<caption>Leave Requests</caption>
+			<thead class="table-primary">
+				<tr>
+					<th id="serial">S.No</th>
+					<th id="employeeId">Id</th>
+					<th id="employeeName">Name</th>
+					<th id="fromDate">From</th>
+					<th id="toDate">To</th>
+					<th id="duration">Duration</th>
+					<th id="status">Status</th>
+					<th id="appliedTime">Applied on</th>
+				</tr>
+			</thead>
+			<tbody id="table-body">
+
+			</tbody>
+		</table>
+	</main>
+	<script type="text/javascript">
+function getAllRequests(){
+	console.log("Fetching");
+	let url = "RequestStatusServlet2";
+	fetch(url).then(res => res.json()).then(res=>{
+		let requests = res;
+		console.log(requests);
+		let content ="";
+		let serial = 1;
+		for(let request of requests){
+			console.log(request.fromDate);
+			content += 
+			"<tr><td>" + serial + "</td><td>" + request.employeeName + "</td><td>" + request.employeeId + "</td><td>" + request.fromDate + "</td><td>" + request.toDate +"</td><td>" + request.duration + "</td><td>" + request.status + "</td><td>" + request.appliedTime + "</td></tr>";
+			console.log(content);
+			serial++; 
+		}
+		
+		document.querySelector("#table-body").innerHTML = content;
+	})
+}
+getAllRequests();
+</script>
+</body>
+</html>
